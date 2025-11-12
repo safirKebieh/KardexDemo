e86a03ad1a249a16a6996bb03f045f263cbe28a3
Safir Kebieh <safeer-k@hotmail.com>
2025-11-12 19:18:37 +0100

    refactor: remove Domain layer and PalletId value type as it’s not required for the current demo scope-FOLDER

--- Domain/PalletId.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Domain
-{
-    public readonly record struct PalletId(string Value)
-    {
-        public override string ToString() => Value;
-        public static bool IsValid(string v) => !string.IsNullOrWhiteSpace(v);
-    }
-}
